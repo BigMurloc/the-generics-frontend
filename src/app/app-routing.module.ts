a134e1432bf4a2b1bb6466c5932623d1bd355aaf
BigMurloc <s21159@pjwstk.edu.pl>
2021-04-05 19:47:14 +0200

    store

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {AboutComponent} from './pages/about/about.component';
 import {HomeComponent} from './pages/home/home.component';
+import {StoreComponent} from './pages/store/store.component';
 
 const routes: Routes = [
   {path: 'about', component: AboutComponent},
-  {path: 'home', component: HomeComponent}
+  {path: 'home', component: HomeComponent},
+  {path: 'store', component: StoreComponent}
 ];
 
 @NgModule({
