154d450c08fd0c899f0d365f83c173b94a2eec4d
BigMurloc <s21159@pjwstk.edu.pl>
2021-04-06 13:30:38 +0200

    refactor order

--- src/app/components/order/order.component.ts
+++ src/app/components/order/order.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Product} from '../../models/product';
+import {Subject} from 'rxjs';
 
 @Component({
   selector: 'app-order',
@@ -7,9 +9,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrderComponent implements OnInit {
 
-  constructor() { }
+  totalPrice = 0;
+  products!: Product[];
+
+
+  constructor() {
+  }
 
   ngOnInit(): void {
+    this.products = [
+      {
+        name: 'T-Shirt',
+        price: 14.99,
+        img: '/assets/images/Shirt.png',
+        quantity: 0
+      },
+      {
+        name: 'Album 3',
+        price: 10.99,
+        img: '/assets/images/Album 3.png',
+        quantity: 0
+      },
+      {
+        name: 'Album 2',
+        price: 12.99,
+        img: '/assets/images/Album 2.png',
+        quantity: 0
+      },
+      {
+        name: 'Album 1',
+        price: 20.99,
+        img: '/assets/images/Album 1.png',
+        quantity: 0
+      }
+    ];
+  }
+
+  calculateTotalPrice(e: any, productName: string): void {
+    let currentPrice = 0;
+    for (const product of this.products) {
+      if (product.name === productName) {
+        product.quantity = e.data;
+      }
+      currentPrice += product.price * product.quantity;
+    }
+    this.totalPrice = currentPrice;
   }
 
 }
